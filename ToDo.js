c57b10f7bb7bda7bd23b0729b2081874eb1fde7f
pierre265 <kevinngako@gmail.com>
2024-03-13 20:54:06 +0100

    Js code and modif CSS

--- ToDo.js
+++ ToDo.js
@@ -5,11 +5,85 @@ const error = document.getElementById("error");
 const countValue = document.querySelector("count-value");
 
 
-let TaskCount = 0;
+let taskCount = 0;
 
-const displayCount = (TaskCount) => {
-    countValue.innerText = TaskCount;
+const displayCount = (taskCount) => {
+    countValue.innerText = taskCount;
 };
 
+const addTask = () =>{
+    const taskName = newTaskInput.ariaValueMax.trim();
+    error.style.display = "none";
+    
+    if(!taskName){
+        setTimeout(()=>{
+            error.style.display ="block" ;
+        },200);
+        
+        return;
+    }
+
+    const task = `<div class="task">
+        <input type="checkbox" class="task-check">
+        <span class="taskname">${taskName}</span>
+        <button class="edit">
+        <i class="fa-solid fa-pen-to-square"></i>
+        </button>
+        <button class="delete">
+        <i class="fa-solid fa-trash"></i>
+        </button>
+        </div>`;
+
+    TaskContainer.insertAdjacentHTML('beforeend', task);
+
+    const deleteButtons = document.querySelectorAll(".delete");
+    deleteButtons.forEach(button => {
+        button.onclick = () =>{
+            button.parentNode.remove();
+            taskCount -= 1;
+            displayCount(taskCount);
+        };
+    });
+
+    const editButtons = document.querySelectorAll('.edit');
+    editButtons.forEach((editBtn) => {
+        editButtons.onclick = (e) =>{
+            let targetElement = e.target;
+            if (!(e.target.className == "edit")) {
+                targetElement = e.target.parentElement;
+            }
+            newTaskInput.value = targetElement.previousElementSibling?.innerText;
+            targetElement.parentNode.remove();
+            taskCount -= 1;
+            displayCount(taskCount);
+        };
+    });
+    
+
+    const taskCheck = document.querySelectorAll('.task-check');
+    taskCheck.forEach((checkBox) => {
+        checkBox.onChange = () =>{
+            checkBox.nextElementSibling.classList.toggle("completed");
+            if (checkBox.checked) {
+                taskCount -= 1;
+            }else taskCount += 1;
+            displayCount(taskCount);
+        };
+    });
+    taskCount += 1;
+    displayCount(taskCount);
+};
+
+addBtn.addEventListener("click", addTask);
+
+window.onload = () =>{
+    taskCount = 0;
+    displayCount(taskCount);
+    newTaskInput.value = "";
+}
+
+// // delete a task from the list
+// Tasks.prototype.delTask = function(e){
+//     if(e.target.classList.contains('delete')){  
 
 
